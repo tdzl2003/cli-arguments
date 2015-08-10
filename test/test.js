ea315ea7865c86bd1f46edd515fcde8110eba2ad
DengYun <tdzl2003>
2015-08-10 20:35:29 +0800

    support parse argument from string or array

---
+++ test/test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by tdzl2_000 on 2015-08-10.
+ */
+"use strict";
+
+var cliArgv = require("../lib/index.js");
+
+console.log(cliArgv.parse({}, 'hello world'));
+console.log(cliArgv.parse({}, 'hello "world"'));
+console.log(cliArgv.parse({}, 'hello "hello world"'));
+console.log(cliArgv.parse({}, 'hello "hello "" world"'));
+console.log(cliArgv.parse({}, 'hello "hello "" world'));
+console.log(cliArgv.parse({}, 'hello "hello " world'));
+console.log(cliArgv.parse({}, 'hello"hello "world'));
+console.log(cliArgv.parse({}, 'hello world""'));
