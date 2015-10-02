eb192aa495801202ad93001336cd40281ca59c1b
DengYun <tdzl2003>
2015-10-02 08:58:33 +0800

    remove useless babel.

--- test/test.js
+++ test/test.js
@@ -2,15 +2,6 @@
  * Created by tdzl2_000 on 2015-08-10.
  */
 "use strict";
-
-require("babel/register")({
-    "ignore": false,
-    "whitelist": [
-        "es6.arrowFunctions",
-        "es6.destructuring"
-    ]
-});
-
 var cliArgv = require("../lib/index.js");
 
 console.log(cliArgv.parse({}, 'hello world'));
